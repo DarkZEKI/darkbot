f8d936fbd7bdcbd2a9e372aed74706be9449a78a
DarkZEKI <81875205+DarkZEKI@users.noreply.github.com>
2021-04-20 17:07:20 -0500

    Update badut.sh

--- badut.sh
+++ badut.sh
@@ -76,7 +76,7 @@ sleep 2
 git clone https://github.com/Darkziinh/SITEX
 git clone https://github.com/Darkziinh/apikey
 git clone https://github.com/Darkziinh/testek
-cd darkbot3.0
+cd darkbot
 chmod +x git.sh
 echo "$verde"
 echo "Agora tem 2 etapas de senhas...."
